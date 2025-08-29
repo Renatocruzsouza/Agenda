47ac72a11db86fce0a589f432cc638189623efec
Renato <renatosouzacruz263@gmail.com>
2025-08-29 13:20:29 -0300

    feat: add initial agenda page with Bootstrap, Font Awesome and CSS integration

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+ include_once("config/url.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agenda de Contatos</title>
+    <!-- BOOTSTRAP -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/css/bootstrap-reboot.min.css" integrity="sha512-99XlObZBl5dbDC1T5YLUShB5GxlnFpilkme4bwFVENsJkTvb0fkagArXjEkA/iBSFCuCfuicaqJO/dOm3Cb//g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+     <!-- FONT AWESOME -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+      <!-- CSS -->
+       <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+</head>
+<body>
+    <h1>Testando Agenda</h1>
+    <i class="fas fa-eye"></i>
+</body>
+</html>
