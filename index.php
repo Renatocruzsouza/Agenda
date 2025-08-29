8aa1c2cb7b5289aec6c5e6e49a100bbbed3af17f
Renato <renatosouzacruz263@gmail.com>
2025-08-29 16:06:37 -0300

    feat: add contacts listing with table and actions in home page

--- index.php
+++ index.php
@@ -1,26 +1,40 @@
 <?php
-
- include_once("config/url.php");
-
+    include_once("templates/header.php")
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Agenda de Contatos</title>
-    <!-- BOOTSTRAP -->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/css/bootstrap-reboot.min.css" integrity="sha512-99XlObZBl5dbDC1T5YLUShB5GxlnFpilkme4bwFVENsJkTvb0fkagArXjEkA/iBSFCuCfuicaqJO/dOm3Cb//g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-     <!-- FONT AWESOME -->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
-      <!-- CSS -->
-       <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
-</head>
-<body>
-    <h1>Testando Agenda</h1>
-    <i class="fas fa-eye"></i>
-</body>
-</html>
+    <div class="container">
+        <?php if(isset($printMsg) && $printMsg != ''):?>
+            <p id="msg"><?= $printMsg ?></p>
+        <?php endif; ?>
+        <h1 id="main-title">Minha Agenda</h1>
+        <?php if(count($contacts) > 0): ?>
+        <table class="table" id="contacts-table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Telefone</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($contacts as $contact): ?> 
+                    <tr>
+                        <td scope="row"><?= $contact["id"] ?></td>
+                        <td scope="row"><?= $contact["name"] ?></td>
+                        <td scope="row"><?= $contact["phone"] ?></td>
+                        <td class="actions">
+                            <a href="#"><i class="fas fa-eye check icon"></i></a>
+                            <a href="#"><i class="far fa-edit edit icon"></i></a>
+                            <button type="submit"><i class="fas fa-times delete-icon"></i></button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>   
+            </tbody>
+        </table>
+        <?php else: ?>
+        <p id="empy-list-text">Ainda não há contatos na sua agenda, <a href="<?= $BASE_URL ?>create.php">Clique aqui para adicionar</a>. </p>
+        <?php endif; ?>
+    </div>
+<?php
+    include_once("templates/footer.php")
+?>
