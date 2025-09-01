09e263085088de9914fc0c6cd1e3b7cd864cc184
Renato <renatosouzacruz263@gmail.com>
2025-09-01 14:32:05 -0300

    feat: fetch contact by id and return contact data from database

--- config/process.php
+++ config/process.php
@@ -5,6 +5,33 @@ session_start();
 include_once("connection.php");
 include_once("url.php");
 
+$id;
+
+if(!empty($_GET)) {
+    $id = $_GET["id"];
+}
+
+// retorna o dado de um contato
+
+if(!empty($_GET)) {
+
+    $query = "SELECT * FROM contacts WHERE id = :id";
+
+    $stmt = $conn->prepare($query);
+
+    $stmt->bindParam("id", $id);
+
+    $stmt->execute();
+
+    $contact = $stmt->fetch();
+
+} else {
+
+}
+
+// retorna todos os contatos
+$contacts = [];
+
 $query = "SELECT * FROM contacts";
 
 $stmt = $conn->prepare($query);
